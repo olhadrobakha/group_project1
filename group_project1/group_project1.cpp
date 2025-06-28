2759e3019c79f4ef08040a3aa08f61b7f4ca4c36
olhadrobakha <olhadrobakha@gmail.com>
2025-06-28 22:08:03 +0300

    added some code

--- group_project1/group_project1.cpp
+++ group_project1/group_project1.cpp
@@ -1,5 +1,106 @@
 #include <iostream>
+#include <string>
+using namespace std;
+struct User {
+	string name;
+	string forname;
+	int id;
+	string email;
+};
+struct Task{
+	
+};
+struct Category {
+
+};
+Task* newTask(string title, string description, string deadline, int priority, bool done, enum type, User user) {
+	Task* task = new Task;
+	task->title = title;
+	task->description = description;
+	task->deadline = deadline;
+	task->priority = priority;
+	task->done = done;
+	task->type = type;
+	task->user = user;
+	return task;
+}
+
+Task** createTaskArray(int size,int taskCount,enum type, User user) {
+	Task** arr = new Task * [size];
+	
+	for (int i = 0; i < taskCount; ++i) {
+		for (int i = 0; i < taskCount && i < size; ++i) {
+			string title, description, deadline, type;
+			int priority;
+			bool done = false;
+
+			cout << "Enter task title #" << i + 1 << ": ";
+			cin.ignore();
+			getline(cin, title);
+
+			cout << "Enter description: ";
+			getline(cin, description);
+
+			cout << "Enter deadline: ";
+			getline(cin, deadline);
+
+			cout << "Do you want to add one more task?(1-yes,2-no):" << endl;
+			int option;
+			cin >> option;
+
+
+			arr[i] = newTask(title, description, deadline, taskCount, done, type, user);
+			if (option == 2) {
+				break;
+			}
+	}
+	return arr;
+}
+void deleteTaskAt(Task * *arr, int& taskCount, int index) {
+		if (index < 0 || index >= taskCount) {
+			cout << "Invalid index to delete!"<<endl;
+			return;
+		}
+		delete arr[index];
+
+		for (int i = index; i < taskCount - 1; ++i) {
+			arr[i] = arr[i + 1];
+		}
+		arr[taskCount - 1] = nullptr; 
+		taskCount--; 
+	}
 void main()
 {
-  
+	int id = 1;
+	User user;
+	user.id = id;
+	id++;
+	const int maxTasks = 100;
+	int tasksCount = 0;
+	
+
+	cout << "Choice an option: 1-add task, 2-delete task, 3-mark done" << endl;
+	switch (option) {
+	case 1:
+		Task * *tasks = createTaskArray(maxTasks, tasksCount);
+		tasksCount++;
+		break;
+	case 2:
+		int index;
+		cout << "Enter the index of the task to delete: ";
+		cin >> index;
+		deleteTaskAt(tasks, taskCount, index);
+		break;
+	}
+	case 3:
+		int num;
+		cout << "Enter the index of the task to mark as done: ";
+		cin >> num;
+		if (num < 0 || num >= tasksCount) {
+			cout << "Invalid task index!" << endl;
+		}
+		else {
+			tasks[num]->done = true;
+			cout << "Task #" << num << " marked as done." << endl;
+		}
 }
